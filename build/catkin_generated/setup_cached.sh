de898d0610e42c940a48b2ea6bc9be95a0027a4e
John Ripple <johnrripple@gmail.com>
2023-01-11 18:55:06 -0700

    Changed catkin_ws to the github repository

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH='/home/john/Human-Robotics/devel:/opt/ros/melodic'
+export LD_LIBRARY_PATH='/home/john/Human-Robotics/devel/lib:/opt/ros/melodic/lib'
+export PKG_CONFIG_PATH='/opt/ros/melodic/lib/pkgconfig'
+export PWD='/home/john/Human-Robotics/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/john/Human-Robotics/devel/share/common-lisp'
+export ROS_PACKAGE_PATH='/home/john/Human-Robotics/src:/opt/ros/melodic/share'
---
+++ build/ripple_tutorials/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/john/Human-Robotics/src/ripple_tutorials
+# Build directory: /home/john/Human-Robotics/build/ripple_tutorials
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
