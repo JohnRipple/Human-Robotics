5c261e0c35db400d2a44e198ebaf0e2529857ae8
John Ripple <johnrripple@gmail.com>
2023-02-01 08:58:56 -0700

    Project1_P2 no longer relies on j to make the M

--- Project1_P2/src/ripple_drawM/scripts/ripple_drawM.py
+++ Project1_P2/src/ripple_drawM/scripts/ripple_drawM.py
@@ -8,6 +8,7 @@ import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
+import math
 
 pose = Pose()   # global pose variable
 
@@ -19,17 +20,28 @@ def callback(data):
 
 def ripple_drawM(bound):
     global pose
-    new_pose = [pose.x + bound[0], pose.y + bound[1]]   # Use an offset distance from the last moved spot
+    
     pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10) # Publish to the topic /turtle1/cmd_vel
     rospy.init_node('ripple_drawM', anonymous=False)    # Initialize the drawM node
-    rate = rospy.Rate(39.8) # 39.8hz Made the M come out fairly well
+    rate = rospy.Rate(40) # 39.8hz Made the M come out fairly well
     msg = Twist()   # Use Twist data type for published data
-    stopx, stopy, j, boundx, boundy = 0, 0, 0, 0, 0
-    
-    while ((stopx == 0 or stopy == 0)):
+
+    msg.linear.x = 0
+    msg.linear.y = 0
+    pub.publish(msg)
+    listener()
+    rospy.loginfo(pose)
+
+    new_pose = [pose.x + bound[0], pose.y + bound[1]]   # Use an offset distance from the last moved spot
+
+    stop, boundx, boundy = 0, 0, 0
+    epsilon = 0.05
+    d = math.sqrt((new_pose[0] - pose.x)**2+(new_pose[1] - pose.y)**2)  # distance 
+    dprev = d
+    while ((stop == 0)):
         sign = 1
         listener()  # Get the current pose of the turtle
-
+        d = math.sqrt((new_pose[0] - pose.x)**2+(new_pose[1] - pose.y)**2)
         # Normalize the offset values to use for speed values
         if (bound[1] != 0):
             boundy = abs(bound[1] / bound[1])
@@ -38,20 +50,18 @@ def ripple_drawM(bound):
         if (bound[0] != 0 and bound[1] != 0):
             boundx = abs(bound[0]/ bound[1])
 
-        # Check a boundary around the new pose to see if the turtle has reached the location yet
-        if ((pose.y > new_pose[1] + 0.1 or pose.y < new_pose[1] - 0.1) and bound[1] != 0 ):
+        if (d > epsilon and dprev >= d):
             if bound[1] < 0:
                 sign = -1
             msg.linear.y = boundy * 3 * sign
-        else:
-            stopy = 1
-        sign = 1
-        if ((pose.x > new_pose[0] + 0.1 or pose.x < new_pose[0] - 0.1) and bound[0] != 0):
+            sign = 1
             if bound[0] < 0:
                 sign = -1
             msg.linear.x = boundx * 3 * sign
         else:
-            stopx = 1
+            stop = 1
+
+        dprev = d
         #rospy.loginfo(pose)
         pub.publish(msg)    # Publish the x,y speed information
         rate.sleep()
@@ -71,16 +81,18 @@ def listener():
 
 if __name__ == '__main__':
     try:
-        bounds = [[5.0, 8.0], [0, -3], [-0.4, 0], [0, -1], [2, 0], [0, 1], [-0.4, 0], [0, 6], [0.4, 0], [0, 1], [-2, 0], [-1.875, -3.0]]    # x,y bounds 0 means it doesn't change
-        
+        bounds = [[3.0*5.0/8.0, 3.0], [0, -3], [-0.4, 0], [0, -1], [2, 0], [0, 1], [-0.4, 0], [0, 4], [0.4, 0], [0, 1], [-2, 0], [-3.0*5.0/8.0, -3.0]]    # x,y bounds 0 means it doesn't change
+        #bounds = [[5.54+1.9, 5.54+3.0]]
         # Draw half the M
         for i in bounds:
+            listener()
             ripple_drawM(i)
 
         # Draw the mirrored half of the M
         for i in bounds[::-1]:
             ripple_drawM([i[0], -1 * i[1]])
-        ripple_drawM([0.63, 1]) # Close a small gap where both halfs of the M meet
+        ripple_drawM([0.39, -0.39*8.0/5.0])
+        ripple_drawM([0.39, 0.39*8.0/5.0]) # Close a small gap where both halfs of the M meet
 
     except rospy.ROSInterruptException:
         pass
