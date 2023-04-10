df0cc5b11d97a869ac5e519407f2f46785bf2f59
John Ripple <johnrripple@gmail.com>
2023-04-10 08:41:51 -0600

    Created Project3

---
+++ Project3_P1/build/ripple_skeleton/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/john/Human-Robotics/Project3_P1/src/ripple_skeleton
+# Build directory: /home/john/Human-Robotics/Project3_P1/build/ripple_skeleton
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
