98a8b54abd902784f750e207449f48e40072ce60
John Ripple <johnrripple@gmail.com>
2023-04-12 14:23:09 -0600

    Project 3 Part 1 Probably finished

--- Project3_P1/src/ripple_skeleton/src/ripple_skeleton.py
+++ Project3_P1/src/ripple_skeleton/src/ripple_skeleton.py
@@ -15,15 +15,25 @@ from matplotlib import pyplot as plt
 class Skeleton:
 
     def __init__(self):
-        self.distance = []
-        self.angle = []
-        self.data = []
-        self.writeData = ""
-        self.nan = False
-        self.hjpd_vec = []
+        self.distance = []  # RAD distances between hip joint and extremities
+        self.angle = []     # RAD angle between hip to extremeties vectors
+        self.data = []      # Read in line from a file
+        self.writeData = "" # Data to write to the rad_d1_XX.txt file
+        self.nan = False    # NaN flag for a frame
+        self.hjpd_vec_x = []    # The x distances calculated for every joint in each frame
+        self.hjpd_vec_y = []
+        self.hjpd_vec_z = []
         self.hjpd_data = []
+        self.hjpdWriteData = "" # Data to write to the cust_d1_xx.txt file
+
+        # Read in the parameter from the roslaunch file
+        try:
+            self.train = rospy.get_param('/ripple_skeleton/train')
+        except:
+            self.train = False
         
     def showHist(self, hist, bins):
+        '''Display a histogram calulated with numpy.histogram'''
         width = 0.7 * (bins[1] - bins[0])
         center = (bins[:-1] + bins[1:]) / 2
         plt.bar(center, hist, align='center', width=width)
@@ -31,8 +41,7 @@ class Skeleton:
         
         
     def calcHistogram(self):
-        '''Create Histogram based on data'''
-        
+        '''Create Histogram based on data for RAD algorithim'''
         dist = np.array(self.distance).T    # Transpose array to get each row as a set of distances from hip center to one extremity
         angle = np.array(self.angle).T
         
@@ -51,7 +60,24 @@ class Skeleton:
         self.writeData += '\n'
         #print(self.writeData)
         
-        
+    
+    def calHJPDHist(self):
+        '''Calculate the histograms for the x, y, z distances for every joint for all frames in a file'''
+        data_x = np.array(self.hjpd_vec_x).T
+        data_y = np.array(self.hjpd_vec_y).T
+        data_z = np.array(self.hjpd_vec_z).T
+        for i in range(0, len(data_x)):
+            hist_x, bins_x = np.histogram(data_x[i], bins=np.linspace(min(data_x[i]), max(data_x[i]), num = 20))
+            hist_y, bins_y = np.histogram(data_y[i], bins=np.linspace(min(data_y[i]), max(data_y[i]), num = 20))
+            hist_z, bins_z = np.histogram(data_z[i], bins=np.linspace(min(data_z[i]), max(data_z[i]), num = 20))
+            hist_x = hist_x / float(len(data_x[i]))
+            hist_y = hist_y / float(len(data_y[i]))
+            hist_z = hist_z / float(len(data_z[i]))
+            # self.showHist(hist_x, bins_x) 
+            self.hjpdWriteData += ' '.join(map(str, hist_x)) + ' '.join(map(str, hist_y)) + ' '.join(map(str, hist_z))
+        self.hjpdWriteData += '\n'
+
+
     def unit_vector(self, vector):
         """ Returns the unit vector of the vector.  """
         if np.linalg.norm(vector):
@@ -99,7 +125,7 @@ class Skeleton:
         # self.distance = []
         
     def stripData(self, line):
-        '''Converts read in line into list of floats while removing Nan frames'''
+        '''Converts read in line into list of floats while flagging Nan frames'''
         line = line.strip("\r\n").split(" ")
         if '' in line:
             # Removes extra whitespaces in data in case the line had two spaces next to each other
@@ -117,24 +143,34 @@ class Skeleton:
 
 
     def calcJointDisplace(self):
+        '''Find the distance between the hip center joint and all other joints'''
         ref_joint = 1   # Position 0 in self.data
-        hjpd_vec = []
+        hjpd_vec_x = [] # All the x distances for every joint in a frame
+        hjpd_vec_y = []
+        hjpd_vec_z = []
         # print(self.hjpd_data)
         for i in range(1, len(self.hjpd_data)):
-            hjpd_vec.append(self.hjpd_data[i][2] - self.hjpd_data[0][2], self.hjpd_data[i][3] - self.hjpd_data[0][3], self.hjpd_data[i][4] - self.hjpd_data[0][4]))
-        print(hjpd_vec)
-        self.hjpd_vec.append(hjpd_vec)
+            hjpd_vec_x.append(self.hjpd_data[i][2] - self.hjpd_data[0][2])
+            hjpd_vec_y.append(self.hjpd_data[i][3] - self.hjpd_data[0][3])
+            hjpd_vec_z.append(self.hjpd_data[i][4] - self.hjpd_data[0][4])
+        self.hjpd_vec_x.append(hjpd_vec_x)
+        self.hjpd_vec_y.append(hjpd_vec_y)
+        self.hjpd_vec_z.append(hjpd_vec_z)
+        # hjpd_vec = [Frame 1: [joint1, joint2, joint3... ],
+        # Frame 2: [joint1, joint2, joint3...]
+        # ...]
 
 
     def hjpd(self, line):
+        '''Find the distance from hip center to all joints for the HJPD algorithim'''
         data = self.stripData(line)
-        self.hjpd_data.append(data)
+        self.hjpd_data.append(data) # Append all joint data
         if data[1] == 20:
             if self.nan:
-                self.nan = False
+                self.nan = False    # Don't add the Nan frame
                 print(self.hjpd_data)
             else:
-                self.calcJointDisplace()
+                self.calcJointDisplace()    # Calc the distnace between joints
             self.hjpd_data = []
 
 
@@ -144,42 +180,44 @@ class Skeleton:
         data = self.stripData(line)
         # print(data)
         if data[1] in desired_joints:
-            self.data.append(data)
+            self.data.append(data)  # Add joint data only if is one of the desired joints
         if data[1] == 20:
             # Don't add this frame of data to the histograms if it has NaNs in the frame
             if not self.nan:
                 self.calcDistance() # Calculate distance between joints
                 self.calcAngle()    # Calculate angle between joints
             self.data = []
-            self.frame = data[0]
     
     
     def mainLoop(self):
-        train = True
-        if train:
+        if self.train:
             dataset_path = "train"
-            save_path = "src/ripple_skeleton/saved_files/rad_d1_train.txt"
         else:
             dataset_path = "test"
-            save_path = "src/ripple_skeleton/saved_files/rad_d1_test.txt"
-        path = os.path.join(os.getcwd(), "src/ripple_skeleton/dataset/", dataset_path)
-        
+        save_path = os.path.join((__file__).replace("ripple_skeleton.py", ""), os.pardir, "saved_files/", "rad_d1_" + dataset_path + ".txt")
+        save_path_cust = os.path.join((__file__).replace("ripple_skeleton.py", ""), os.pardir,"saved_files/","cust_d1_" + dataset_path +".txt")
+        path = os.path.join((__file__).replace("ripple_skeleton.py", ""), os.pardir, "dataset/", dataset_path)
+        print(save_path)
         open(save_path, 'w').close()    # Clear contents of the file
         save_file = open(save_path, 'w')
+        open(save_path_cust, 'w').close()    # Clear contents of the file
+        save_file_cust = open(save_path_cust, 'w')
         for filename in os.listdir(path):
             print(filename)
             self.distance = []  # Reset distance and angle for new file
             self.angle = []
             self.writeData = ""
+            self.hjpdWriteData = ""
             with open(os.path.join(path, filename), 'r') as f:
-                self.frame = 1
                 for i, line in enumerate(f):
-                    self.findStar(line)
-                    self.hjpd(line)
-                self.calcHistogram()
+                    self.findStar(line) # RAD algorithim
+                    self.hjpd(line)     # HJPD algorithim
+                self.calcHistogram()    # Calculate the RAD histogram
+                self.calHJPDHist()      # Calculate the HJPD histogram
                 save_file.write(self.writeData)
-                
+                save_file_cust.write(self.hjpdWriteData)
         save_file.close()
+        save_file_cust.close()
 
     
 
